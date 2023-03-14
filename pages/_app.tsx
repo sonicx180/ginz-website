643454748419f718986eaf05640fabbd84014b16
Sonicx180 <chetanzeogu410@gmail.com>
2023-03-13 20:41:05 -0700

    sass errors are so annoying

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-
+import Head from 'next/head'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <meta charSet = "utf-8" />
+        <meta name = "description" content = "Ginz & Associates is an incoporation for Engineers, Contractors, and more created by Godfrey Nzeogu" />
+         
+    </Head>   
+    <Component {...pageProps} />
+    </>
+  )
 }
